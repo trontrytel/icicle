ebe5da08622583bb179584931f87a4872f8c80a4
slayoo <slayoo@igf.fuw.edu.pl>
2012-02-10 01:06:41 +0100

    draft of __float128 support (not working yet, and hence commented out)

---
+++ src/mdl_128.cpp
@@ -0,0 +1,17 @@
+/** @file
+ *  @author Sylwester Arabas <slayoo@igf.fuw.edu.pl>
+ *  @copyright University of Warsaw
+ *  @date February 2012
+ *  @section LICENSE
+ *    GPL v3 (see the COPYING file or http://www.gnu.org/licenses/)
+ */
+
+#include "cfg.hpp"
+#include "cmn.hpp"
+#include "mdl.hpp"
+
+void mdl_128(const po::variables_map &vm, const string &options) 
+{
+  // TODO: error if not available with the current compiler...
+  mdl<__float128>(vm, options);
+}
