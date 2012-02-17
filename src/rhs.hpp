df836dce792f97c0020d518c19d2c50721f4d597
slayoo <sarabas@igf.fuw.edu.pl>
2012-02-17 17:16:16 +0100

    wip on source terms

---
+++ src/rhs.hpp
@@ -0,0 +1,24 @@
+/** @file
+ *  @author Sylwester Arabas <slayoo@igf.fuw.edu.pl>
+ *  @copyright University of Warsaw
+ *  @date Februaryy 2012
+ *  @section LICENSE
+ *    GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+ */
+#ifndef RHS_HPP
+#  define RHS_HPP
+
+#  include "cmn.hpp" 
+#  include "mtx.hpp"
+
+template <typename real_t>
+class rhs 
+{
+  public: virtual void operator()(
+    mtx::arr<real_t> &R, 
+    mtx::arr<real_t> **psi, 
+    mtx::idx &ijk
+  ) = 0;
+};  
+
+#endif
