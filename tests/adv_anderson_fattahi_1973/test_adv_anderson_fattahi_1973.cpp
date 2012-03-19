2dc3037bb459833552ea4bec39e0120271b8b470
trontrytel <jaruzanka@gmail.com>
2012-03-19 18:25:51 +0100

    new name for rotating cone test

---
+++ tests/adv_anderson_fattahi_1973/test_adv_anderson_fattahi_1973.cpp
@@ -0,0 +1,24 @@
+/** @file
+ *  @example adv_anderson_fattahi_1973/test_test_adv_anderson_fattahi_1973.cpp
+ *  @author Anna Jaruga <ajaruga@igf.fuw.edu.pl>
+ *  @author Sylwester Arabas <slayoo@igf.fuw.edu.pl>
+ *  @copyright University of Warsaw
+ *  @date November 2011
+ *  @section LICENSE
+ *    GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
+ *  @section DESCRIPTION
+      Rotating cone test from Anderson and Fattahi 1973 J. Atmos. Sci.
+ *  @section RESULTS
+ *    \include "adv_anderson_fattahi_1973/test_adv_anderson_fattahi_1973.py"
+ *    \image html "../../tests/adv_anderson_fattahi_1973/fig-leapfrog-2d.gif"
+ *    \image html "../../tests/adv_anderson_fattahi_1973/fig-upstream-2d.gif"
+ *    \image html "../../tests/adv_anderson_fattahi_1973/fig-mpdata-2d.gif"
+ */
+
+#include <cstdlib>
+
+int main() 
+{ 
+  int status = system("python test_adv_anderson_fattahi_1973.py");
+  exit(status == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
