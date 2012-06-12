36eaf18af13b88afe73c9aeb5175401f7120eb4e
slayoo <sarabas@igf.fuw.edu.pl>
2012-06-12 16:21:19 +0200

    MPI: separating into .hpp and .cpp

--- src/slv_parallel_distmem_mpi.hpp
+++ src/slv_parallel_distmem_mpi.hpp
@@ -6,58 +6,44 @@
  *    GPLv3+ (see the COPYING file or http://www.gnu.org/licenses/)
  */
 #pragma once
-//TODO: split into .hpp and .cpp
-#  ifdef USE_BOOST_MPI
+#include "slv_parallel_distmem.hpp"
+#include "slv_parallel_serial.hpp"
 
-#  include "slv_parallel_distmem.hpp"
-
+#ifdef USE_BOOST_MPI
 #  include <boost/mpi.hpp>
 namespace mpi = boost::mpi;
+#endif
 
 template <typename real_t, class shrdmem_class>
 class slv_parallel_distmem_mpi : public slv_parallel_distmem<real_t, shrdmem_class>
 {
+#ifdef USE_BOOST_MPI
   private: enum msgtype { msg_halo };
   private: mpi::environment *env; // perhaps MPI::Init_thread(MPI_THREAD_MULTIPLE) instead to support MPI+threads?
   private: mpi::communicator *comm;
-
-  /// MPI init for using rank as an argument to the parent class c-tor
-  private: int mpi_init()
-  {
-    env = new mpi::environment(); 
-    comm = new mpi::communicator();
-    return comm->rank();
-  }
+  private: int mpi_init();
+  public: ~slv_parallel_distmem_mpi();
+  private: void sndrcv(int peer, int cnt, real_t *ibuf, real_t *obuf);
+#endif
 
   public: slv_parallel_distmem_mpi(
     const stp<real_t> &setup, 
     out<real_t> &output, 
     int nsd
-  ) : slv_parallel_distmem<real_t, shrdmem_class>(setup, output, nsd, mpi_init())
+  ) 
+#ifdef USE_BOOST_MPI
+  ;
+#else
+  : slv_parallel_distmem<real_t, shrdmem_class>(setup, output, nsd, 0)
   {
-    if (nsd != comm->size()) 
-      error_macro("MPI is using different number of nodes (" << comm->size() << ") than specified (" << nsd << ")")
+    error_macro("please recompile icicle with -DMPI")
   }
-
-  public: ~slv_parallel_distmem_mpi()
-  {
-    delete comm;
-    delete env; 
-  }
+#endif
 
   private: void distmem_barrier()
-  {
-    comm->barrier();
-  }
-
-  private: void sndrcv(int peer, int cnt, real_t *ibuf, real_t *obuf)
-  {
-    mpi::request reqs[2];
-    int r = 0;
-    // isend() / irecv() are non-blocking
-    reqs[r++] = comm->isend(peer, msg_halo, obuf, cnt); 
-    reqs[r++] = comm->irecv(peer, msg_halo, ibuf, cnt);
-    mpi::wait_all(reqs, reqs + r);
-  }
+#ifdef USE_BOOST_MPI
+  ;
+#else
+  {}
+#endif
 };
-#  endif
