7141ec09d6af8e1e44b8fd34eb7929fd54bea3bc
slayoo <slayoo@igf.fuw.edu.pl>
2012-08-23 13:22:26 +0200

    fixing odeint search in CMakeLists.txt + ODEINT=NO compilation

--- src/sdm/sdm_openmp.cpp
+++ src/sdm/sdm_openmp.cpp
@@ -1,5 +1,6 @@
 #include "../cfg/cfg_thrust.hpp"
-#if defined(_OPENMP) && defined(USE_THRUST)
+#include "../cfg/cfg_boost_odeint.hpp"
+#if defined(_OPENMP) && defined(USE_THRUST) && defined(USE_BOOST_ODEINT)
 #  define THRUST_DEVICE_SYSTEM THRUST_DEVICE_SYSTEM_OMP
 #  define ICICLE_THRUST_DEVICE_SYSTEM sdm::openmp
 #  include "sdm.cpp"
