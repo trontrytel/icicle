5f381a39302027613b219a78b07733791845b7e1
slayoo <slayoo@igf.fuw.edu.pl>
2013-08-18 16:52:34 +0200

    async as cmd line option; fig_a cosmetic changes (incl. reflectivity plot)

--- tests/fig_a/plot_blk_1m.cpp
+++ tests/fig_a/plot_blk_1m.cpp
@@ -6,26 +6,25 @@ int main(int ac, char** av)
 {
   if (ac != 2) error_macro("expecting 1 argument: CMAKE_BINARY_DIR")
 
-  string file = string(av[1]) + "/tests/fig_a/out_blk_1m.h5";
+  std::string 
+    dir = string(av[1]) + "/tests/fig_a/",
+    h5  = dir + "out_blk_1m.h5",
+    svg = dir + "out_blk_1m.svg";
 
   Gnuplot gp;
-  gp << "set term svg size 700, 350 fname \"Verdana\" fsize 9 \n";
-  // progressive-rock connoisseur palette ;)
-  gp << "set palette defined (0 '#FFFFFF', 1 '#993399', 2 '#00CCFF', 3 '#66CC00', 4 '#FFFF00', 5 '#FC8727', 6 '#FD0000')\n";
-  gp << "set view map\n";
+  init(gp, svg, 1, 2);
 
-  gp << "set output '" << av[1] << "/tests/fig_a/blk_1m.svg'\n";
-  gp << "set multiplot layout 1,2\n";
+  {
+    auto rhod_rc = h5load(h5, "rhod_rc") * 1e3;
+    gp << "set title 'liquid water content [g/m^3]'\n"; 
+    gp << "set cbrange [0:1.5]\n";
+    plot(gp, rhod_rc);
+  }
 
-  gp << "set xlabel \"x/dx\"\n";
-  gp << "set ylabel \"y/dy\"\n";
-
-  auto rhod_rc = h5load(file, "rhod_rc") * 1000;
-  gp << "set title \"rhod_rc [g/m3]\" \n"; 
-  plot(gp, rhod_rc);
-  auto rhod_rr = h5load(file, "rhod_rr") * 1000;
-  gp << "set title \"rhod_rr [g/m3]\" \n"; 
-  plot(gp, rhod_rr);
-
-  gp << "set term x11\n";
+  {
+    auto rhod_rr = h5load(h5, "rhod_rr") * 1e3;
+    gp << "set title 'rain water content [g/m^3]'\n"; 
+    gp << "set cbrange [0:1]\n";
+    plot(gp, rhod_rr);
+  }
 }
