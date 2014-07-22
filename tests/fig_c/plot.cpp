71a2a05687b3fdaa7cf6e5b6b1f1222cb18112d4
slayoo <slayoo@igf.fuw.edu.pl>
2014-07-22 12:36:28 +0200

    fixing fig_c test

--- tests/fig_c/plot.cpp
+++ tests/fig_c/plot.cpp
@@ -11,7 +11,7 @@ int main(int ac, char** av)
 
   using namespace icmw8_case1;
 
-  float dx = (X / si::metres) / n["x"], dz = (Z / si::metres) / n["z"];
+  float dx = (X / si::metres) / (n["x"]-1), dz = (Z / si::metres) / (n["z"]-1);
 
   blitz::Array<float, 2> tmp(n["x"], n["z"]);
 
@@ -22,18 +22,18 @@ int main(int ac, char** av)
 
   std::vector<std::vector<float>> v(4);
 
-  tmp = (ix + .5) / n["x"] * (X / si::metres) / 20; 
+  tmp = (ix + .5) / n["x"] * (n["x"] - 1);
 
   for (const auto &i : tmp) v[0].push_back(i);
   
-  tmp = (jx + .5) / n["z"] * (Z / si::metres) / 20;
+  tmp = (jx + .5) / n["z"] * (n["z"] - 1);
 
   for (const auto &i : tmp) v[1].push_back(i);
 
   tmp = - A * ( 
     psi((ix+.5)/n["x"], (jx+.5+.5)/n["z"])-
     psi((ix+.5)/n["x"], (jx+.5-.5)/n["z"])
-  ) / dz                // numerical derivative
+  ) / dz                  // numerical derivative
   / rhod()((jx+.5) * dz); // psi defines rho_d times velocity
 
   for (const auto &i : tmp) v[2].push_back(i);
@@ -46,9 +46,10 @@ int main(int ac, char** av)
 
   for (const auto &i : tmp) v[3].push_back(i);
 
-  gp << "scl = 3.33\n";
-  gp << "set xrange [0:75]\n";
-  gp << "set yrange [0:75]\n";
+  gp << "scl = .666\n";
+  gp << "set title 'momentum field'\n";
+  gp << "set xrange [0:" << n["x"]-1 << "]\n";
+  gp << "set yrange [0:" << n["z"]-1 << "]\n";
   gp << "set cbrange [0:5]\n";
   gp << "splot '-' using 1:2:(0):(scl*$3):(scl*$4):(0):(sqrt($3**2 + $4**2)) with vectors linecolor rgbcolor variable notitle\n";
   gp.send(v);
