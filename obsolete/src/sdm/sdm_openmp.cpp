6f1956c440dee20742fe65fb44b96455f4621a43
slayoo <slayoo@igf.fuw.edu.pl>
2015-01-06 14:27:06 +0100

    deleting obsolete directory

--- obsolete/src/sdm/sdm_openmp.cpp
+++
@@ -1,7 +0,0 @@
-#include "../cfg/cfg_thrust.hpp"
-#include "../cfg/cfg_boost_odeint.hpp"
-#if defined(_OPENMP) && defined(USE_THRUST) && defined(USE_BOOST_ODEINT)
-#  define THRUST_DEVICE_SYSTEM THRUST_DEVICE_SYSTEM_OMP
-#  define ICICLE_THRUST_DEVICE_SYSTEM sdm::openmp
-#  include "sdm.cpp"
-#endif
